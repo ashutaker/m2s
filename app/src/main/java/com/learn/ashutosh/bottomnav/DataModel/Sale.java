3237903518e49e029d8902b74cbbb75c8c645528
ashutosh <ashutosh.kanojia.ak@gmail.com>
2018-08-22 04:00:37 +0530

    Room_Database with product listing added.

--- app/src/main/java/com/learn/ashutosh/bottomnav/DataModel/Sale.java
+++ app/src/main/java/com/learn/ashutosh/bottomnav/DataModel/Sale.java
@@ -1,4 +1,49 @@
 package com.learn.ashutosh.bottomnav.DataModel;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+import java.util.Date;
+
+import static android.arch.persistence.room.ForeignKey.CASCADE;
+
+@Entity(tableName = "sale",
+        foreignKeys = {@ForeignKey(entity = Customer.class,
+                parentColumns = "id",
+                childColumns = "customerId",
+                onDelete = CASCADE),
+                @ForeignKey(entity = Product.class,
+                        parentColumns = "id",
+                        childColumns = "productId",
+                        onDelete = CASCADE)})
+
 public class Sale {
+
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private int customerId;
+
+    private int productId;
+
+    private String shippingType;
+
+    //private String AWB;
+
+    private Date saleDate;
+
+    public Sale() {
+    }
+
+    @Ignore
+    public Sale(int customerId, int productId, String shippingType, Date saleDate) {
+        this.customerId = customerId;
+        this.productId = productId;
+        this.shippingType = shippingType;
+        this.saleDate = saleDate;
+    }
 }
